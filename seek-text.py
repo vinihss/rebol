dab74449e4398972f1e2d976a8d7e099689dbc35
Vinícius Heleno <vinihss@gmail.com>
2025-02-05 16:58:34 -0300

    Create seek-text.py

---
+++ seek-text.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-785d0f016f9444a4b142e5107e35a585", base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
